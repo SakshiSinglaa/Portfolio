d91b74d173a18f8665a30b953f48dd9c2de21d32
Portfolio <sakshisingla2803@gmail.com>
2025-01-14 19:56:02 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sakshisinglaa.github.io/Portfolio/"
+  base: "/Portfolio/"
 })
